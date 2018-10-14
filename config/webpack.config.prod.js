4805ab35be6ee80efeab085243d9a57c3c59870f
Muyunyun <328375795@qq.com>
2018-10-14 14:34:40 +0800

    feat: css模块babel插件

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -142,7 +142,7 @@ module.exports = {
               {
                 loader: require.resolve('less-loader'),
                 options: {
-                  modifyVars: { "@primary-color": "#1890ff" },
+                  modifyVars: { '@primary-color': '#1890ff' },
                 },
               },
             ],
